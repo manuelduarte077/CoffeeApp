354d9c47833bdfad008d29510c564332a04bb48b
Manuel Duarte  <carlosd99m@gmail.com>
2023-02-25 22:47:19 -0600

    create ui

--- app/src/main/java/com/getnerdify/coffeeapp/MainActivity.kt
+++ app/src/main/java/com/getnerdify/coffeeapp/MainActivity.kt
@@ -3,56 +3,31 @@ package com.getnerdify.coffeeapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.material.TextField
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.lifecycle.ViewModelProvider
 import com.getnerdify.coffeeapp.ui.theme.CoffeeAppTheme
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        var dataManager = ViewModelProvider(this)
+            .get(DataManager::class.java)
+
         setContent {
             CoffeeAppTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
+                    modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background
                 ) {
-                    DefaultPreview()
+                    App(dataManager)
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    var name = remember {
-        mutableStateOf("")
-    }
-
-    Column() {
-        Text(
-            "Hello ${name.value}", modifier = Modifier
-                .padding(16.dp)
-                .background(Color.Yellow)
-                .padding(16.dp)
-        )
-        TextField(value = name.value, onValueChange = {
-            name.value = it
-        }, modifier = Modifier.padding(16.dp))
-    }
-}
